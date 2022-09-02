b4e33e1dc613fbcfe2899ca70f07fbbe8d8e1e42
cyy <3163795095@qq.com>
2022-09-02 16:37:10 +0800

    test

--- 上传.bat
+++ 上传.bat
@@ -1,9 +1,12 @@
 @echo off 
 echo File is being uploaded, please wait...... 
 git add .
+set time="1235456798"
 git commit -m "test"
+@echo %time%
 git push
 echo File uploaded successfully ! 
 del /f /s /q "C:\Users\caoyangyang\AppData\Roaming\Typora\typora-user-images\*.*"
 echo Prcture Clear successfully ! 
+pause
 exit
