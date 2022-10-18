1f2ca7c6f6f8424e4caacef73cdd82866fa06ac7
cyy <3163795095@qq.com>
2022-10-18 19:30:19 +0800

    2022/10/18 周二  19:30:19.48

--- 同步.bat
+++ 同步.bat
@@ -2,5 +2,8 @@
 echo File is being uploaded（git pull）, please wait...... 
 cd H:\note
 git pull
+
+cd E:\Program Files (x86)\Typora\
+start Typora.exe
 echo File uploaded successfully（git pull） ! 
 exit
